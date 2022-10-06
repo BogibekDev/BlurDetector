97ebf83334d3314bd3941cecae500ffb9663eec6
bogibek <bogibekdev@gmail.com>
2022-10-06 17:20:58 +0500

    class and fun are added

--- app/src/main/java/dev/bogibek/blurdetector/MainActivity.kt
+++ app/src/main/java/dev/bogibek/blurdetector/MainActivity.kt
@@ -1,11 +1,37 @@
 package dev.bogibek.blurdetector
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.squareup.picasso.Picasso
+import dev.bogibek.blurdetector.databinding.ActivityMainBinding
+import dev.bogibek.blurdetector.utils.BlurrinessDetector
+import kotlinx.coroutines.*
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initViews()
+
     }
+
+    private fun initViews() {
+        binding.apply {
+            bIsBlurred.setOnClickListener {
+                val imageUrl = if (etUrl.text.isNotEmpty()) etUrl.text.toString() else ""
+                val imageBitmap = Picasso.get().load(imageUrl).get()
+                if (isBlurred(imageBitmap)) tvResult.text = "This image is blurred"
+                else tvResult.text = "This image is not blurred"
+            }
+        }
+    }
+
+    private fun isBlurred(imageBitmap: Bitmap): Boolean {
+        return runBlocking{ BlurrinessDetector.isBlurredPhoto(imageBitmap) }
+    }
+
 }
